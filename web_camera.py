47521a7ab2b1f323d893038c0b377d489f57e34c
your_github_username <jakub.galuszka@interia.pl>
2023-07-31 23:39:50 +0200

    First results

--- web_camera.py
+++ web_camera.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 
-model=YOLO('runs/detect/train11/weights/best.pt')
+model=YOLO('runs/detect/train/weights/best.pt')
 results = model.predict(source='0', show=True)
 
 print(results)
